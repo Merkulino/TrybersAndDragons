02738d18590d471f0506cd13806b3a83099d03c3
Melqui Brito <Merkulino11@gmail.com>
2023-05-05 16:13:12 -0300

    Feat: Requisito 1 e iniciando o T&D 🧙‍♂️

---
+++ src/Races/Race.ts
@@ -0,0 +1,21 @@
+abstract class Race {
+  constructor(private _name: string, private _dexterity: number) {}
+  
+  get name() {
+    return this._name;
+  }
+
+  get dexterity() {
+    return this._dexterity;
+  }
+
+  static createdRacesInstances():number { // Eu não entendi KKKK
+    // if (instances > maxInstances) throw new Error('Not Implemented');
+    // return instances + 1;
+    throw new Error('Not implemented');
+  }
+
+  abstract get maxLifePoints():number;
+}
+
+export default Race;
